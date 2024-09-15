71598e32892f3882fb8ba0a823579d9f84bfd00c
macDad <madjay87@gmail.com>
2024-09-15 11:33:23 -0400

    Initial commit

--- src/test/java/com/aplicy/qa/automation/canadawanderlust/CWApplicationCoreTests.java
+++ src/test/java/com/aplicy/qa/automation/canadawanderlust/CWApplicationCoreTests.java
@@ -6,6 +6,8 @@ import java.time.Duration;
 
 import com.aplicy.qa.automation.DriverUtil;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.WebDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,7 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class CWApplicationCoreTests {
 
   WebDriver driver;
-  String url = "https://www.canadawanderlust.com";
+  String mainUrl = "https://www.canadawanderlust.com";
 
   @BeforeEach
   @DisplayName("Set up the Chrome WebDriver in headless mode before each test")
@@ -26,9 +28,9 @@ class CWApplicationCoreTests {
 
   @Test
   @DisplayName("Verify that the CanadaWanderlust home page title is correct")
-  void login() {
+  void homePageTitle() {
     String expectedTitle = "CanadaWanderlust | Best Travel Destinations & Tips | Explore Canada.";
-    driver.get(url);
+    driver.get(mainUrl);
     String actualTitle = driver.getTitle();
     driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
     assertEquals(expectedTitle, actualTitle);
@@ -40,7 +42,7 @@ class CWApplicationCoreTests {
     @Test
     @DisplayName("Verify that the Destinations page title is correct")
     void exploreDestinationsPageTitle() {
-      driver.get(url + "/travel-destinations");
+      driver.get(mainUrl + "/travel-destinations");
       String expectedTitle = "Travel Destinations | Explore the World's Best Spots with Canada Wanderlust";
       assertEquals(expectedTitle, driver.getTitle());
     }
@@ -50,19 +52,11 @@ class CWApplicationCoreTests {
   @DisplayName("Test Adventure Trips Page")
   class AdventureTripsTests {
 
-    @Test
+    @ParameterizedTest(name = "Verify that the Adventure Trip for {1} loads correctly at {0}")
+    @CsvFileSource(resources = "/__file/cw-destination-data.csv", numLinesToSkip = 1)
     @DisplayName("Verify that the Adventure Trips page loads correctly")
-    void adventureTripsPage() {
-      driver.get(url + "/destination/summer-fun-niagara-falls-top-activities");
-      String expectedTitle = "Explore Canada with CanadaWanderlust | Best Travel Destinations & Tipse";
-      assertEquals(expectedTitle, driver.getTitle());
-    }
-
-    @Test
-    @DisplayName("Verify that the The Best Beaches in Cape Breton")
-    void theBestBeachesInCapeBretonPage() {
-      driver.get(url + "/destination/best-beaches-cape-breton");
-      String expectedTitle = "Explore Canada with CanadaWanderlust | Best Travel Destinations & Tipse";
+    void adventureTripsPage(String url, String expectedTitle) {
+      driver.get(mainUrl + "/destination/" + url);
       assertEquals(expectedTitle, driver.getTitle());
     }
   }
@@ -74,7 +68,7 @@ class CWApplicationCoreTests {
     @Test
     @DisplayName("Verify Contact Page Title")
     void contactPageTitle() {
-      driver.get(url + "/contact/");
+      driver.get(mainUrl + "/contact/");
       String expectedTitle = "Contact Us | Canada Wanderlust - Your Ultimate Travel Guide";
       assertEquals(expectedTitle, driver.getTitle());
     }
